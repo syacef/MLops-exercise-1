f1346f3b3dd7bd4c6080750b0d239fac83d0ca26
Samy YACEF <samy.yacef@epita.fr>
2025-09-10 19:35:17 +0200

    uvicorn

--- model_web_server.py
+++ model_web_server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import joblib
+import uvicorn
 
 app = FastAPI()
 
@@ -10,3 +11,7 @@ def predict(data: dict):
     features = [[data["size"], data["nb_rooms"], data["garden"]]]
     prediction = model.predict(features)
     return {"y_pred": prediction[0]}
+
+
+if __name__ == "__main__":
+    uvicorn.run("model_web_server:app", host="0.0.0.0", port=8000, reload=True)
